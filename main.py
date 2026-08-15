b78cb6f4d8ed46ba9bfae6974d9e778ff2ebdf5a
Sean <seanjcarley@gmail.com>
2026-08-15 18:36:30 +0100

    started woking on processing results using bs4 and commented out calls on main to make testing process quicker.

--- main.py
+++ main.py
@@ -15,35 +15,45 @@ def main():
     max_search_date = dt(max_year, max_month, max_day)
     min_search_date = dt(2017, 6, 1)
 
-    def format_date(date):
+    def format_date(date_type):
         ''' extract day, month, year from date provided in DD/MM/YYYY format '''
-        try:
-            year, month, day = int(date[6:]), int(date[3:5]), int(date[0:2])
+
+        if date_type == 'end':
+            search_date = dt(int(start_date[:4]), int(start_date[5:7]), int(start_date[8:]))
+        else:
+            search_date = min_search_date
+
+        valid = False
+
+        while not valid:
+            try:
+                date = input(f'Please enter the {date_type} date in the DD/MM/YYYY format: ')
+                year, month, day = int(date[6:]), int(date[3:5]), int(date[0:2])
+
+                f_date = dt(year, month, day)
+
+                if search_date <= f_date <= max_search_date:
+                    f_date = dt(year, month, day)
+                    valid = True
+                else:
+                    print(f'{date} is not within the range {search_date.strftime("%d/%m/%Y")} - {max_search_date.strftime("%d/%m/%Y")}')
+            except ValueError as e:
+                print(f'The date provided {date} was not in the correct DD/MM/YYYY format!')
+                print('Please reenter the date!')
             
-            date = dt(year, month, day)
+        return f_date.strftime("%Y-%m-%d")
 
-            if (today < date) or (date <  min_search_date):
-                revised_date = (f'The date provided is invalid. Please enter date between 01/06/2017 and {str(today.strftime("%d/%m/%Y"))}: ')
-                format_date(revised_date)
-            
-            return date
+    # ########################################################################
+    # print('Data is available from June 2017 to today...')
+    # start_date = format_date('start')
+    # end_date = format_date('end')
+    # ########################################################################
 
-        except ValueError as e:
-            # print(e)
-            revised_date = input(f'The date was not provided in the correct format. Please enter the date in DD/MM/YYY format: ')
-
-
-    print('Data is available from June 2017 to today...')
-    start_date = input('Please enter the start date in the DD/MM/YYYY format: ')
-    st_date = format_date(start_date)
-    end_date = input('Please enter the end date in the DD/MM/YYYY format: ')
-    ed_date = format_date(end_date)
-
-    print(st_date, ed_date)
+    # print(start_date, end_date)
     
 
 
-    # data1 = gr.GetReport('2023-01-01', '2023-01-31').get_report_data()
+    # data1 = gr.GetReport(start_date, end_date).get_report_data()
     # data2 = gr.GetReport('2023-02-01', '2023-02-28').get_report_data()
     # print(data)
 
@@ -52,6 +62,7 @@ def main():
     # print(soup.prettify())
 
     # pr.ProcessReport(data1).make_soup()
+    pr.ProcessReport().make_soup()
 
 if __name__ == '__main__':
     main()
--- process_report.py
+++ process_report.py
@@ -1,17 +1,59 @@
 from bs4 import BeautifulSoup
 import time as t
-import datetime as dt
+from datetime import datetime as dt
 
 class ProcessReport:
 
-    def __init__ (self, report):
-        self.report = report
+    # def __init__ (self, report):
+        # self.report = report
+    def __init__ (self):
+        self.report = open('test.html')
+        self.months = {'january': 1, 'february': 2, 'march': 3, 
+                    'april': 4, 'may': 5, 'june': 6, 
+                    'july': 7, 'august': 8, 'september': 9, 
+                    'october': 10, 'november': 11, 'december': 12}
+        self.events = {'qc failure ': 1, 'qc outlier': 2, 'qc atypical': 3,
+                       'events': 4, 'special': 5, 'holiday': 6, 
+                       'offline': 7, 'oeekends and defined holidays': 8, 
+                       'holiday-affected days': 9}
 
 
     def make_soup(self):
         soup = BeautifulSoup(self.report, 'html.parser')
 
-        for item in soup.select('tbody tr td'):
-            print(item)
+        mbk, car, lgv, bus, hgr, hga, cvn, ivd = [], [], [], [], [], [], [], []
+
+        for tr in soup.select('tbody tr'):
+            entry = []
+            for td in tr.select('td'):
+                if not td.has_attr('class'):
+                    if td.has_attr('rowspan'):
+                        if int(str(td).split('"')[1]) == 16:
+                            td_str = str(td).replace('<td rowspan="16">', '').replace('</td>', '')
+                            date_str =  td_str.split(' ')
+                            month_str = date_str[2].lower()
+                            date = dt(int(date_str[3]), self.months[month_str], int(date_str[1]))
+                            # print(date.strftime("%Y-%m-%d"))
+                            entry.append(date)
+                
+                if td.has_attr('class'):
+                    # print(td.attrs['class'], td.attrs['data-a'])
+                    values = td.attrs['data-a'].split(',')
+                    if len(td.attrs['class']) == 0:
+                        entry.append(0)
+                    else:
+                        event = self.events[td.attrs['class'][0]]
+                    entry.append(event, values[0], values[2], values[3])
+
+            print(entry)
+
+
+
+                
+
+
+
+                    
+                
             
             
