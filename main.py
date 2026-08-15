062931bbc7bfe190efa56b48fc4d196e8bf3b4c0
Sean <seanjcarley@gmail.com>
2026-08-15 22:17:00 +0100

    make_soup function now processes html and return list with processed data

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import get_report as gr
 import process_report as pr
-from bs4 import BeautifulSoup
-import datetime as d
 from datetime import datetime as dt
 
 
 def main():
+    # get the current date
     today = dt.now()
+
+    # set the max search terms so that future dates will not be submitted
     max_year = today.year
     max_month = today.month
     max_day = today.day
-    # max_hour = today.hour
 
     max_search_date = dt(max_year, max_month, max_day)
-    min_search_date = dt(2017, 6, 1)
+    min_search_date = dt(2017, 6, 1)  # the oldest data is from this date
 
     def format_date(date_type):
         ''' extract day, month, year from date provided in DD/MM/YYYY format '''
 
+        # ensuring that the end date submitted by the user is not before the
+        # start data already submitted
         if date_type == 'end':
             search_date = dt(int(start_date[:4]), int(start_date[5:7]), int(start_date[8:]))
         else:
@@ -27,42 +29,47 @@ def main():
 
         while not valid:
             try:
+                # get the date from the user
                 date = input(f'Please enter the {date_type} date in the DD/MM/YYYY format: ')
                 year, month, day = int(date[6:]), int(date[3:5]), int(date[0:2])
 
                 f_date = dt(year, month, day)
 
+                # make sure that the user supplied date is on or after the
+                # earliest date that data is available for and not a future 
+                # date
                 if search_date <= f_date <= max_search_date:
                     f_date = dt(year, month, day)
                     valid = True
                 else:
+                    # if it is advise the user that the date is not within the
+                    # allowed date range and ask to resubmit the date
                     print(f'{date} is not within the range {search_date.strftime("%d/%m/%Y")} - {max_search_date.strftime("%d/%m/%Y")}')
             except ValueError as e:
+                # capture exception if invalid date format is entered and ask
+                # the user to reenter the date in the correct format
                 print(f'The date provided {date} was not in the correct DD/MM/YYYY format!')
                 print('Please reenter the date!')
             
         return f_date.strftime("%Y-%m-%d")
 
     # ########################################################################
-    # print('Data is available from June 2017 to today...')
-    # start_date = format_date('start')
-    # end_date = format_date('end')
+    # request the date range from the user
+    print('Data is available from June 2017 to today...')
+    start_date = format_date('start')
+    end_date = format_date('end')
     # ########################################################################
 
-    # print(start_date, end_date)
+    # print(start_date, end_date)  # used for testing
     
-
-
-    # data1 = gr.GetReport(start_date, end_date).get_report_data()
+    # create scrape object
+    data1 = gr.GetReport(start_date, end_date).get_report_data()
     # data2 = gr.GetReport('2023-02-01', '2023-02-28').get_report_data()
-    # print(data)
+    # print(data)  # used to test scrape
 
-    # return data
-    # soup = BeautifulSoup(data1, 'html.parser')
-    # print(soup.prettify())
-
-    # pr.ProcessReport(data1).make_soup()
-    pr.ProcessReport().make_soup()
+    # create report processing object
+    pr.ProcessReport(data1).make_soup()
+    # pr.ProcessReport().make_soup()  # to be used for testing using local html file
 
 if __name__ == '__main__':
     main()
--- process_report.py
+++ process_report.py
@@ -1,59 +1,78 @@
 from bs4 import BeautifulSoup
-import time as t
-from datetime import datetime as dt
 
 class ProcessReport:
+    ''' 
+        html from get_report an parse it using Beautiful Soup to create data
+        to be entered to data base so that there is a local copy rather than
+        repeatedly accessing the website
+    '''
 
-    # def __init__ (self, report):
-        # self.report = report
-    def __init__ (self):
-        self.report = open('test.html')
+    def __init__ (self, report):  # omit report for testing
+        self.report = report  # use for testing open('test.html')
+    # def __init__ (self): 
+        # self.report = 
+        # dictionary to map string to int for the months
         self.months = {'january': 1, 'february': 2, 'march': 3, 
                     'april': 4, 'may': 5, 'june': 6, 
                     'july': 7, 'august': 8, 'september': 9, 
                     'october': 10, 'november': 11, 'december': 12}
-        self.events = {'qc failure ': 1, 'qc outlier': 2, 'qc atypical': 3,
-                       'events': 4, 'special': 5, 'holiday': 6, 
-                       'offline': 7, 'oeekends and defined holidays': 8, 
-                       'holiday-affected days': 9}
+        # dictionary to map string to int for the events that could impact volumes
+        self.events = {'qc-fail': 1, 'qc-atypical': 2, 'qc-outlier': 3,
+                       'road-event': 4, 'special-event': 5, 'holiday': 6, 
+                       'offline': 7, 'weekend': 8, 'holiday-affected days': 9}
+        # dictionary to map string to int for the days
+        self.days = {'monday': 1, 'tuesday': 2, 'wednesday': 3, 'thursday': 4, 
+                     'friday': 5, 'saturday': 6, 'sunday':7}
 
 
     def make_soup(self):
-        soup = BeautifulSoup(self.report, 'html.parser')
+        ''' 
+            take the html from get_report.py to prepare it to be added to the 
+            database 
+        '''
+        soup = BeautifulSoup(self.report, 'html.parser')  # create a BeautifulSoup object
 
-        mbk, car, lgv, bus, hgr, hga, cvn, ivd = [], [], [], [], [], [], [], []
+        events = []
 
-        for tr in soup.select('tbody tr'):
-            entry = []
+        # the data to be used is in the Table Body element on the page
+        for tr in soup.select('tbody tr'):  # each table row contains a different vehicle class for each day and direction
             for td in tr.select('td'):
-                if not td.has_attr('class'):
-                    if td.has_attr('rowspan'):
-                        if int(str(td).split('"')[1]) == 16:
-                            td_str = str(td).replace('<td rowspan="16">', '').replace('</td>', '')
-                            date_str =  td_str.split(' ')
-                            month_str = date_str[2].lower()
-                            date = dt(int(date_str[3]), self.months[month_str], int(date_str[1]))
-                            # print(date.strftime("%Y-%m-%d"))
-                            entry.append(date)
-                
-                if td.has_attr('class'):
-                    # print(td.attrs['class'], td.attrs['data-a'])
-                    values = td.attrs['data-a'].split(',')
-                    if len(td.attrs['class']) == 0:
-                        entry.append(0)
+                # the td element that has a rowspan of 16 has the date
+                # the below if statement checks if the current td element and
+                # if it has a rowspan of 16 it retrieves the data
+                if (td.has_attr('rowspan')) and (int(str(td).split('"')[1]) == 16):
+                    td_str = str(td).replace('<td rowspan="16">', '').replace('</td>', '')
+                    date_str =  td_str.split(' ')
+                    year = int(date_str[3])
+                    month = self.months[date_str[2].lower()]
+                    day = int(date_str[1])
+                    dow = self.days[date_str[0].lower()]
+
+                # the td elements that have class and data-a attributes contain
+                # the count for that specific direction, hour, day, vehicle 
+                # class
+                if (td.has_attr('class')) and (td.has_attr('data-a')):
+                    td_str = str(td).split('"')
+                    if td_str[1] == '':
+                        # week days with no event have a class that is 
+                        # interpreted as an empty string, so to avoid having an 
+                        # empty string as a key in the dict, just assign int(0)
+                        # to the list
+                        event_class = 0
                     else:
-                        event = self.events[td.attrs['class'][0]]
-                    entry.append(event, values[0], values[2], values[3])
+                        # check the events dict for the int value to represent
+                        # the event, change to lowercase to match the key
+                        event_class = self.events[td_str[1].lower()]
+                    data_a = td_str[3].split(',')  # split the data-a attr
+                    direction = int(data_a[0])
+                    hour = int(data_a[2])
+                    vehicle_class = int(data_a[3])
+                    # extract the vehicle count for the particular hour
+                    vehicle_count = int(td_str[4].replace('>', '').replace('</td', ''))
 
-            print(entry)
+                    # add the processed data to the events list to be used to 
+                    # add the data to the database
+                    events.append([year, month, day, dow, hour, event_class, direction, vehicle_class, vehicle_count])
 
+        return events
 
-
-                
-
-
-
-                    
-                
-            
-            
